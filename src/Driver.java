507baed79cfdc55a66675fd94c430b482b17a15e
Lomar Lilly <lithiumlilly3@gmail.com>
2017-03-20 14:59:48 -0500

    validate sentence
    Had to change out the implementation on sorting the dataset on insert. SO now I'm using bubble sort to sort the linked list after the dataset is loaded into the linked list.

--- src/Driver.java
+++ src/Driver.java
@@ -7,12 +7,12 @@ public class Driver {
 		
 		lList.loadAndSortDataset();
 		
-		lList.addWord("Abbreviation", "n.", "A good ute");
-		
-		System.out.print("\n\nLength of the list: "+lList.lengthOfList()+"\n\n");
-		
+		lList.bubbleSort();
+		lList.addWord("Paul", "n.", "A good ute");
+		System.out.println(lList.lookUp("Occasional")); 
+		System.out.print("\nLength of the list: "+lList.lengthOfList()+"\n");
+		lList.validateSentence("Paul is a good yute");
+		System.out.println(lList.lookUp("Good")); 
 		//lList.displayDictionary();
-		
-		System.out.println(lList.lookUp("Zxander")); 
 	}
 }
--- src/LinkedList.java
+++ src/LinkedList.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import gui.fileChooser;
 
@@ -36,35 +38,84 @@ public class LinkedList {
 	}
 	public void loadAndSortDataset(){
 		long StartTime =0;
+		long EndTime =0;
 		try{
 			fileChooser file = new fileChooser();
 			FileInputStream fstream = new FileInputStream(file.openFile());
-			StartTime = System.currentTimeMillis();
+			StartTime = System.nanoTime();
 			DataInputStream in = new DataInputStream(fstream);
 			BufferedReader br = new BufferedReader(new InputStreamReader(in));
 			String strLine;
 			while ((strLine = br.readLine()) != null){
-				this.addToDictionary(strLine);
+				this.Dictionary(strLine);
 			}
 			in.close();
 		}catch (Exception e){
 			System.err.println("Error: " + e.getMessage());
 		}
-		long EndTime = System.currentTimeMillis();
-		  
-		System.out.println("Linked List Inserstion Sort Took: "+(EndTime - StartTime)+" milliseconds\n");
+		EndTime = System.nanoTime();
+		System.out.println("Loading dataset from file to linked list took: "+(EndTime - StartTime)+" nanoseconds");
 	}
 	
-	public void addToDictionary(String wordMeaning){
-		//implements insertion sort
+	public void Dictionary(String wordMeaning){
 		if(isFull()){
 			System.out.println("Error - Cannot insert the item");
 		}else{
 			Node newWordMeaning = new Node(wordMeaning);
+			Node temp = new Node();
+			if(isEmpty()){
+				this.HEAD = newWordMeaning;
+			}
+			else{
+				temp = this.HEAD;
+			    while ((temp.getNextWordMeaning() != null)) {
+			           temp = temp.getNextWordMeaning();
+			    }
+			    temp.setNextWordMeaing(newWordMeaning);
+			}
+			length++;
+		}
+	}
+	
+	public void bubbleSort() {
+		long StartTime = 0;
+		long EndTime = 0;
+		if (isEmpty() || this.HEAD.getNextWordMeaning() == null){
+	        System.out.println("An empty list is already sorted."); 
+		}else {
+			StartTime = System.nanoTime();
+	        Node current = this.HEAD;
+	        boolean swapDone = true;
+	        while (swapDone) {
+	        	swapDone = false;
+	            while (current != null) {
+	            	if(current.getNextWordMeaning() != null && current.getWordMeaning().compareTo(current.getNextWordMeaning().getWordMeaning()) >0){
+	                    String temp = current.getWordMeaning();
+	                    current.setWordMeaning(current.getNextWordMeaning().getWordMeaning());
+	                    current.getNextWordMeaning().setWordMeaning(temp);
+	                    swapDone = true;
+	                }
+	                current = current.getNextWordMeaning();
+	            }
+	            current = this.HEAD;
+	        }
+	    	EndTime = System.nanoTime();
+	    }
+		System.out.println("Sorting the linked list took: "+(EndTime - StartTime)+" nanoseconds");
+	}
+	
+	public void addToDictionary(String wordMeaning){
+		long StartTime = 0;
+		long EndTime = 0;
+		//implements insertion sort
+		if(isFull()){
+			System.out.println("Error - Cannot insert the item");
+		}else{
+			StartTime = System.nanoTime();
+			Node newWordMeaning = new Node(wordMeaning);
 			Node currPtr = new Node();
 			Node temp = new Node();
 			boolean insert = false;
-			
 			if(isEmpty()){
 				this.HEAD = newWordMeaning;
 			}
@@ -92,6 +143,8 @@ public class LinkedList {
 				}
 			}
 			length++;
+			EndTime = System.nanoTime();
+			System.out.println("Adding the word '"+wordMeaning.split("\\s")[0]+"' to the linked list took: "+(EndTime - StartTime)+" nanoseconds");
 		}
 	}
 	
@@ -105,16 +158,24 @@ public class LinkedList {
 	}
 	
 	public String lookUp(String searchValue){
+		long StartTime = 0;
+		long EndTime = 0;
 		Node currentPtr = this.HEAD;
+		StartTime = System.nanoTime();
 		while(currentPtr != null){
 			if(currentPtr.getWordMeaning().split("\\s")[0].equals(searchValue)){
 				int index = this.locate(searchValue);
-				return "Found at index: " +Integer.toString(index)+"\n"+currentPtr.getWordMeaning();
+				EndTime = System.nanoTime();
+				System.out.println("Searching for the word '"+searchValue+"' in the linked list took: "+(EndTime - StartTime)+" nanoseconds");
+				return "\nFound at index: " +Integer.toString(index)+"\n"+currentPtr.getWordMeaning();
 			}
 			currentPtr = currentPtr.getNextWordMeaning();
 		}
+			EndTime = System.nanoTime();
+			System.out.println("Searching for the word '"+searchValue+"' in the linked list took: "+(EndTime - StartTime)+" nanoseconds");
 			return "Not Found";
 	}
+	
 	
 	public int locate(String element){
 		int index = 0;
@@ -131,6 +192,41 @@ public class LinkedList {
 			}
 		}
 		return -1;
+		
+	}
+	public void validateSentence(String sentence){
+		long StartTime = 0;
+		long EndTime = 0;
+		ArrayList<String> arr = new ArrayList<String>();
+		String word[] = sentence.split("[\\W]");
+		Scanner in = new Scanner(System.in);
+		int i;
+		int found;
+		StartTime = System.nanoTime();
+		for(i=0;i<word.length;i++){
+			String capitalized = Character.toUpperCase(word[i].charAt(0)) + word[i].substring(1);
+			found = this.locate(capitalized);
+			if(found==-1){
+				arr.add(capitalized);
+			}
+		}
+		EndTime = System.nanoTime();
+		System.out.println("Validaing the sentence took (Linked List): "+(EndTime - StartTime)+" nanoseconds");
+		int j;
+		if(arr.size()!=0){
+			for(j=0;j<arr.size();j++){
+				System.out.println("'"+arr.get(j)+"' was not found in the dictionary, would you like to add it to the database of words?\n y/n");
+				String respond = in.nextLine();
+				if(respond.equals("y")){
+					System.out.println("Enter part of speech: ");
+					String partOfSpeech = in.nextLine();
+					System.out.println("Enter Meaning: ");
+					String meaning = in.nextLine();
+					this.addWord(arr.get(j), partOfSpeech, meaning);
+				}
+			}
+		}
+		in.close();
 	}
 	
 	public void displayWordsInDictionary(){
@@ -142,10 +238,15 @@ public class LinkedList {
 	}
 	
 	public void displayDictionary(){
+		long StartTime = 0;
+		long EndTime = 0;
 		Node currNode = HEAD;
+		StartTime = System.nanoTime();
 		while(currNode != null){
 			currNode.display();
 			currNode = currNode.getNextWordMeaning();
 		}
+		EndTime = System.nanoTime();
+		System.out.println("Displaying all words, part of speech and defintion in the linked list took: "+(EndTime - StartTime)+" nanoseconds");
 	}
 }
