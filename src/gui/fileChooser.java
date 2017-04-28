12bdcaa074dec1f55f6601e19efb4733d0c9975e
Lomar Lilly <lithiumlilly3@gmail.com>
2017-04-28 09:55:24 -0500

    can now access file path beyond this class

--- src/gui/fileChooser.java
+++ src/gui/fileChooser.java
@@ -3,20 +3,38 @@ package gui;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 
-public class fileChooser {
+import classes.LinkedList;
+
+public class fileChooser 
+{
 	
-	public String openFile(){
+	private static String filePath;
+	
+	public static String getFilePath()
+	{
+		return filePath;
+	}
+	
+	public void setFilePath(String filePath)
+	{
+		fileChooser.filePath = filePath;
+	}
+	
+	public String openFile(String DataStructure)
+	{
 		JButton open = new JButton();
 		JFileChooser fc = new JFileChooser();
 		//REMEMBER TO UNCOMMENT THIS PART!!!!
 		//fc.setCurrentDirectory(new java.io.File("user.home"));
 		//REMEMBER TO TAKE THIS PART OUT!
 		fc.setCurrentDirectory(new java.io.File("C:\\Users\\Admin\\Downloads"));
-		fc.setDialogTitle("Import Dictionary Data");
+		fc.setDialogTitle("Import Dictionary Data for "+DataStructure);
 		fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-		if(fc.showOpenDialog(open) == JFileChooser.APPROVE_OPTION){
+		if(fc.showOpenDialog(open) == JFileChooser.APPROVE_OPTION)
+		{
 			
 		}
+		this.setFilePath(fc.getSelectedFile().getAbsolutePath());
 		return fc.getSelectedFile().getAbsolutePath();
 	}
 }
