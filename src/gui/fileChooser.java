41a851baf08493979e58d90c2a9baa248adcc491
Lomar Lilly <lithiumlilly3@gmail.com>
2017-05-01 21:31:53 -0500

    Update fileChooser.java

--- src/gui/fileChooser.java
+++ src/gui/fileChooser.java
@@ -1,3 +1,6 @@
+/*Lomar Lilly 1401375
+ * Darryl Brown 1503803
+ */
 package gui;
 
 import javax.swing.JButton;
@@ -5,36 +8,35 @@ import javax.swing.JFileChooser;
 
 import classes.LinkedList;
 
-public class fileChooser 
-{
+public class fileChooser {
 	
 	private static String filePath;
 	
-	public static String getFilePath()
-	{
+	public static String getFilePath(){
 		return filePath;
 	}
 	
-	public void setFilePath(String filePath)
-	{
+	public void setFilePath(String filePath){
 		fileChooser.filePath = filePath;
 	}
 	
-	public String openFile(String DataStructure)
-	{
+	public String openFile(String DataStructure){
 		JButton open = new JButton();
 		JFileChooser fc = new JFileChooser();
-		//REMEMBER TO UNCOMMENT THIS PART!!!!
-		//fc.setCurrentDirectory(new java.io.File("user.home"));
-		//REMEMBER TO TAKE THIS PART OUT!
-		fc.setCurrentDirectory(new java.io.File("C:\\Users\\Admin\\Downloads"));
-		fc.setDialogTitle("Import Dictionary Data for "+DataStructure);
-		fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-		if(fc.showOpenDialog(open) == JFileChooser.APPROVE_OPTION)
+		try
 		{
-			
+			fc.setCurrentDirectory(new java.io.File("user.home"));
+			fc.setDialogTitle("Import Dictionary Data for "+DataStructure);
+			fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+			if(fc.showOpenDialog(open) == JFileChooser.APPROVE_OPTION){
+				System.out.println("file loaded");
+			}
+			this.setFilePath(fc.getSelectedFile().getAbsolutePath());
 		}
-		this.setFilePath(fc.getSelectedFile().getAbsolutePath());
+		catch(Exception err)
+		{
+			System.out.println(err);
+		}
 		return fc.getSelectedFile().getAbsolutePath();
 	}
 }
